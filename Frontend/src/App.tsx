2644d0e3b280ac11cfda5f968421205779de0967
Damian <damianpietron007@gmail.com>
2024-11-18 12:36:17 +0100

    Implement react router

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,13 +3,19 @@ import './styles/global.css'
 import  Navbar  from './components/Navbar'
 import  Footer  from './components/Footer'
 import  ChatFooter  from './components/ChatFooter'
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
         <Navbar />
-        <ChatFooter />
+        <Routes>
+        <Route path="/" element={<LandingPage />} />
+        </Routes>        
+        {/* <ChatFooter /> */}
         < Footer />
     </div>
   )
