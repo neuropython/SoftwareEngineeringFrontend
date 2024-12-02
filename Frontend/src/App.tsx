b45ca550874e324a1f9d7027f7e5f0accf3096c5
Damian <damianpietron007@gmail.com>
2024-12-02 22:34:49 +0100

    Add about page to react router

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./styles/global.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import ChatPage from "./pages/ChatPage";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,6 +20,8 @@ function App() {
         <Route path="/chat" element={<ChatPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/about" element={<AboutPage />} /> 
+
       </Routes>
       <Footer />
     </div>
