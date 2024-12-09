40e5ce5a7a27581c6e1aa3eb9927cf6f898dd5ce
Damian <damianpietron007@gmail.com>
2024-12-09 21:55:21 +0100

    Add file to track all endpoints.

---
+++ Frontend/api/endpoints.ts
@@ -0,0 +1,15 @@
+const BASE_URL = 'localhost:8080/';
+
+const ENDPOINTS = {
+    AUTH : {
+        LOGIN: '/auth/login',
+        REGISTER: '/auth/register',
+    },
+    USERS: {
+        ME: '/users/me',
+    },
+    CHATS: {
+        ROOM: '/chats/room',
+        MEDIA: '/chats/media',
+    }
+};
