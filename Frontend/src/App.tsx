27a3a132e7683ebd393dc5292e892d54577a2788
Damian <damianpietron007@gmail.com>
2024-12-10 22:17:01 +0100

    Integrate AuthProvider and LoginBar into App component for enhanced authentication flow

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
 import "./styles/global.css";
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/bars/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import ChatPage from "./pages/ChatPage";
 import { Routes, Route } from "react-router-dom";
@@ -8,23 +7,27 @@ import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import AboutPage from "./pages/AboutPage";
+import LoginBar from "./components/bars/loginBar/LoginBar";
+import { AuthProvider } from "./api/AuthContext";
+
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/about" element={<AboutPage />} /> 
-
-      </Routes>
-    </div>
+      <div>
+        <AuthProvider>
+        <LoginBar />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/about" element={<AboutPage />} /> 
+        </Routes>
+        </AuthProvider>
+      </div>
   );
 }
+
 
 export default App;
