6845c5727be23e13912750c4dc7f934cf9445020
Damian <damianpietron007@gmail.com>
2024-12-10 22:15:25 +0100

    Refactor AuthContext to enhance login functionality and add useAuth hook

--- Frontend/src/api/AuthContext.tsx
+++ Frontend/src/api/AuthContext.tsx
@@ -1,9 +1,12 @@
 // AuthContext.tsx
-import React, { createContext, useState, useEffect, ReactNode } from 'react';
+import React, { createContext, useState, useContext, useEffect, ReactNode } from 'react';
+import  loginClient from '../api/auth/login';
+import { useNavigate } from 'react-router-dom';
+
 
 interface AuthContextType {
     isAuthenticated: boolean;
-    login: () => void;
+    login: (username: string, password: string) => Promise<void>;
     logout: () => void;
 }
 
@@ -15,16 +18,26 @@ type AuthProviderProps = {
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const authStatus = localStorage.getItem('is_authenticated') ?? 'false'; 
         setIsAuthenticated(authStatus === 'true');
     }, []);
 
-    const login = () => {
-        localStorage.setItem('is_authenticated', 'true');
-        setIsAuthenticated(true);
-    };
+    const login = async (username: string, password: string) => {
+        const response = await loginClient(username, password);
+        if (response.status === 200) {
+
+          localStorage.setItem('is_authenticated', 'true');
+          setIsAuthenticated(true);
+          navigate('/');
+    
+
+        } else {
+          throw new Error('Invalid credentials');
+        }
+      };
 
     const logout = () => {
         localStorage.removeItem('is_authenticated');
@@ -37,3 +50,11 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
         </AuthContext.Provider>
     );
 };
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (context === undefined) {
+      throw new Error('useAuth must be used within an AuthProvider');
+    }
+    return context;
+  };
